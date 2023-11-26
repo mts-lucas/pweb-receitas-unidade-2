b5af3ef4bbfba872a7312ce446009f5a4c6f42b4
Lucas Mateus <lmateus1067@outlook.com>
2023-11-26 17:49:09 -0300

    adicionando css

--- receitas/src/app/receita3/page.js
+++ receitas/src/app/receita3/page.js
@@ -1,7 +1,6 @@
 "use client"; 
 import React, { useState } from 'react';
 
-
 export default function Movies() {
   const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState([]);
@@ -15,42 +14,32 @@ export default function Movies() {
   };
 
   return (
-    <div>
-      <div>
-        <div>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="mb-4">
+        <div className="flex items-center">
           <input
             type="text"
             placeholder="Digite o título do filme..."
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
+            className="p-2 border border-gray-300 mr-2 text-red-500"
           />
-          <button onClick={handleSearch}>Pesquisar</button>
+          <button onClick={handleSearch} className="p-2 bg-blue-500 text-white">Pesquisar</button>
         </div>
+      </div>
+      
+      <div className="flex flex-row">
         {searchResults && searchResults.length > 0 ? (
           searchResults.map((m) => (
-            <div key={m.imdbID} className="movie-card">
-              <img src={m.Poster} alt={m.Title} className="movie-poster" />
-              <div className="movie-title">{m.Title} ({m.Year})</div>
+            <div key={m.imdbID} className="flex flex-col items-center mb-4 mr-4">
+              <img src={m.Poster} alt={m.Title} className="w-48 mb-2" />
+              <div className="text-center">{m.Title} ({m.Year})</div>
             </div>
           ))
         ) : (
-          <p>Nenhum resultado encontrado.</p>
+          <p className="mt-4">{searchQuery ? 'Nenhum resultado encontrado.' : 'Pesquise um filme.'}</p>
         )}
       </div>
-      <style jsx>{`
-        .movie-card {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          margin-bottom: 20px;
-        }
-        .movie-poster {
-          width: 200px; /* Tamanho padrão para o cartaz do filme */
-        }
-        .movie-title {
-          text-align: center;
-        }
-      `}</style>
     </div>
   );
 }
